8faeef8fbfbe67ce895dbc08a36e467dedcf7530
iulia0103 <iulia.leancu@gmail.com>
2016-12-15 12:31:18 +0100

    add tests, fix errors

---
+++ linter.js
@@ -0,0 +1,14 @@
+var lint = require('mocha-eslint');
+
+var paths = [
+  'src',
+];
+
+var options = {
+  // Note: we saw timeouts with the 2000ms mocha timeout setting,
+  // thus raised to 5000ms for eslint
+  timeout: 5000,  // Defaults to the global mocha `timeout` option
+};
+
+// Run the tests
+lint(paths, options);
--- src/Balloon.js
+++ src/Balloon.js
@@ -4,16 +4,16 @@ import PopSound from './PopSound'
 
 export default class Balloon {
   static colors = [
-    "#1f77b4",
-    "#ff7f0e",
-    "#2ca02c",
-    "#d62728",
-    "#9467bd",
-    "#8c564b",
-    "#e377c2",
-    "#7f7f7f",
-    "#bcbd22",
-    "#17becf"
+    '#1f77b4',
+    '#ff7f0e',
+    '#2ca02c',
+    '#d62728',
+    '#9467bd',
+    '#8c564b',
+    '#e377c2',
+    '#7f7f7f',
+    '#bcbd22',
+    '#17becf'
   ]
 
   constructor(props = {}) {
@@ -42,7 +42,7 @@ export default class Balloon {
 
     container.animate({
       bottom: '100%',
-      left: xdiff + "%"
+      left: xdiff + '%'
     }, (8000 - width * 10))
   }
 
@@ -97,15 +97,15 @@ export default class Balloon {
     const {color, height, width} = this.props
 
     return (`
-      <div class="balloon" id="${this.id}"
-        style="left: 50%; bottom: 0">
-        <div class="bubble"
-          style="background-color: ${color};
+      <div class='balloon' id='${this.id}'
+        style='left: 50%; bottom: 0'>
+        <div class='bubble'
+          style='background-color: ${color};
             height: ${height}px;
             width: ${width}px;
-            border-radius: ${width}px / ${height}px"/>
-        <div class="string"
-          style="margin-left: ${0.5 * width}px; height: ${0.5 * height}px"/>
+            border-radius: ${width}px / ${height}px'/>
+        <div class='string'
+          style='margin-left: ${0.5 * width}px; height: ${0.5 * height}px'/>
       </div>
     `)
   }
